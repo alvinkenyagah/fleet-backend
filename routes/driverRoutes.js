a904f85d2987d8fad649b1897a3fca61c295cb22
alvin kenyagah <alvin47k@gmail.com>
2025-06-07 22:45:39 +0300

    get driver function route

--- routes/driverRoutes.js
+++ routes/driverRoutes.js
@@ -2,6 +2,9 @@
 const express = require('express');
 const router = express.Router();
 const { createDriver } = require('../controllers/driverController');
+const { getAllDrivers } = require('../controllers/driverController');
+const { getDriverById } = require('../controllers/driverController');
+const { getDriverByEmail } = require('../controllers/driverController');
 const { protect, isAdmin } = require('../middleware/authMiddleware');
 
 router.post('/', protect, isAdmin, createDriver);
